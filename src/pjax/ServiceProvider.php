fd5ce66340657e5378c666a46645c032e226bcaf
Tim Groeneveld <tim@timg.ws>
2016-01-13 10:43:26 +1000

    Allow 'valid_containers' to work.

---
+++ src/pjax/ServiceProvider.php
@@ -0,0 +1,29 @@
+<?php namespace timgws\pjax;
+
+
+class ServiceProvider extends \Illuminate\Support\ServiceProvider
+{
+    /**
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__.'/config/pjax.php' => config_path('pjax.php'),
+        ]);
+    }
+
+    /**
+     * Register bindings in the container.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->mergeConfigFrom(
+            __DIR__.'/config/pjax.php', 'pjax'
+        );
+    }
+}
---
+++ src/pjax/config/pjax.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * laravel-pjax configuration
+ */
+
+return [
+
+    'valid_containers' => [
+        // See https://github.com/defunkt/jquery-pjax#usage
+        '#pjax-container', '.content'
+    ]
+
+];
